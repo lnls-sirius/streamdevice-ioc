1853faceccc03d47220683ff97185d49a8e4c7e5
carneirofc <claudiofcarneiro@hotmail.com>
2018-07-30 14:23:36 -0300

    MBTemp .cmd template.

---
+++ mbtemp/server/templates/mbtemp_devices.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+devices = [
+    # MBTemp specifics
+    {
+        'f_name' : "MBTMP1.cmd",
+        'IP_ASYN_PORT_1' : "IPPort1",
+        'IP_ADDR_1' : "10.0.6.63:4161",
+        'MBTEMP_ADDRESS' : "8",
+        'PREFIX_1' : "TEST:MBTemp",
+        'SCAN_RATE' : "2"
+    }
+]
--- mbtemp/server/templates/mbtemp_generate.py
+++ mbtemp/server/templates/mbtemp_generate.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""
+    This template is used to generate the default .cmd iocBoot file.
+    The user sould pass as parameter the destination file the will be generated.
+"""
+
 from string import Template
-from mbtemp_template import mbt_template_cmd as mbt_template_cmd
+from mbtemp_template import mbt_template as mbt_template_cmd
+from mbtemp_devices import devices  as devices
+
 import sys 
 
 if __name__ == "__main__":
-    f_name = "MBTMP.cmd"
-    if len(sys.argv) < 2:
-        print('[WARNING]: The user should pass as paramater the destination file name.')
-    else:
-        if sys.argv[1].endswith(".cmd"):
-            f_name = sys.argv[1]
-        else:
-            print('[ERROR]: The file extension must be .cmd.')
+
 
     EPICS_BASE = "/opt/epics-R3.15.5/base"
     ASYN = "/opt/epics-R3.15.5/modules/asyn4-33"
@@ -22,19 +23,16 @@ if __name__ == "__main__":
     CD = "${TOP}"
      
     # MBTemp specifics
-    IP_ASYN_PORT_1 = "IPPort1"
-    IP_ADDR_1 = "10.0.6.63:4161"
-    MBTEMP_ADDRESS = "8"
-    PREFIX_1 = "TEST:MBTemp"
-    SCAN_RATE = "2"
-    
-    # DCM-SE10
-    DCM_SE_10 = False #  will be generated or not ....
-    IP_ASYN_PORT_2 = "IPPort2"
-    IP_ADDR_2 = "10.0.6.63:17001 UDP"
-    PREFIX_2 = "TEST:DCM_SE-10"
+ 
+    for device in devices:
 
-    if not DCM_SE_10:
+        f_name = device['f_name']
+        IP_ASYN_PORT_1 = device['IP_ASYN_PORT_1']
+        IP_ADDR_1 = device['IP_ADDR_1']
+        MBTEMP_ADDRESS = device['MBTEMP_ADDRESS']
+        PREFIX_1 = device['PREFIX_1']
+        SCAN_RATE = device['SCAN_RATE']
+ 
         res = mbt_template_cmd.safe_substitute(
             EPICS_BASE=EPICS_BASE,
             ASYN=ASYN,
@@ -42,13 +40,12 @@ if __name__ == "__main__":
             ARCH=ARCH,
             STREAM_PROTOCOL_PATH=STREAM_PROTOCOL_PATH,
             CD=CD,
-            IP_ASYN_PORT_1=IP_PORT,
+            IP_ASYN_PORT_1=IP_ASYN_PORT_1,
             IP_ADDR_1=IP_ADDR_1,
             MBTEMP_ADDRESS=MBTEMP_ADDRESS,
             PREFIX_1=PREFIX_1,
             SCAN_RATE = SCAN_RATE)
-    else:
-        pass
-    file = open(f_name, 'w+')
-    file.write(res)
-    file.close()
+        
+        file = open(f_name, 'w+')
+        file.write(res)
+        file.close()
