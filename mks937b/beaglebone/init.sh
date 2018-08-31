81a5ca5db9c39229d2407c29e4f7fba7fc28acf1
carneirofc <claudiofcarneiro@hotmail.com>
2018-08-31 11:55:38 -0300

    mks937b: Beaglebone

--- mks937b/beaglebone/init.sh
+++ mks937b/beaglebone/init.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 # Bind using TCP
-socat -d -d TCP-LISTEN:${SOCAT_PORT},fork,reuseaddr,nodelay,range=${SERVER_IP_ADDR}:${SERVER_MASK} FILE:/dev/ttyUSB0,b115200,rawer,crln
-
-#socat -d -d FILE:/dev/ttyUSB0,b115200,rawer TCP4:10.0.6.38:4161
-
-# Bind using TCP
-#socat -d -d TCP-LISTEN:${SOCAT_PORT},fork,reuseaddr,nodelay,range=${SERVER_IP_ADDR}:${SERVER_MASK} FILE:/dev/ttyUSB0,b115200,rawer,crln
+socat -d -d TCP-LISTEN:${SOCAT_PORT},reuseaddr,fork,nodelay,range=${SERVER_IP_ADDR}:${SERVER_MASK} FILE:${DEVICE},b${BAUDRATE},rawer
