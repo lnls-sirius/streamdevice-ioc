89a2cdb4dcdd23765b7264c532d96bacc07867a3
carneirofc <claudiofcarneiro@hotmail.com>
2018-09-17 10:32:12 -0300

    MKS937b: Docker file

--- mks937b/docker/build.sh
+++ mks937b/docker/build.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-DOCKER_MANTAINER_NAME=lnlscon
-DOCKER_NAME=mks937b
-DOCKER_TAG=latest
-
+. ./conf-env.sh
 docker build -t ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG} .
--- mks937b/docker/run.sh
+++ mks937b/docker/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+. ./conf-env.sh
+echo ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG}
 docker run -d \
-    --network host \ 
-    --name mks937b lnlscon/mks937b:latest
+    --network host \
+    --name ${CONTAINER_NAME} \
+     ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG}
