a851f8e547ff85e194ad8fb653fe50b034b4c111
carneirofc <claudiofcarneiro@hotmail.com>
2019-01-23 12:13:18 -0200

    Better xlsx pre-processing

--- common/__init__.py
+++ common/__init__.py
@@ -5,62 +5,52 @@ import pandas
 
 logger = logging.getLogger()
 handler = logging.StreamHandler()
-formatter = logging.Formatter(
-        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
-SPREADSHEET = os.path.dirname(os.path.realpath(__file__)) + \
-    '/../Redes e Beaglebones.xlsx'
+SPREADSHEET = os.path.dirname(os.path.realpath(__file__)) + '/../Redes e Beaglebones.xlsx'
 
 SHEET_MBTEMP = 'PVs MBTemp'
 SHEET_4UHV = 'PVs Agilent 4UHV'
 SHEET_MKS = 'PVs MKS937b'
+SHEET_COUNTING_PRU = 'PVs Counting PRU'
 
-DATA_MBTEMP = {}
-DATA_4UHV = {}
-DATA_MKS = {}
+class DbData():
+    def __init__(self, sheet_name, ip='IP', aditional_check = None):
+        '''
+        :param sheet_name: xlsx sheet name.
+        :param ip: column name containing the ip address.
+        :param aditional_check: a function that receives row and sheet_name and returns True or False depending on the check result.
+        '''
+        self.data = {}
+        sheet = pandas.read_excel(SPREADSHEET, sheet_name=sheet_name, dtype=str)
+        sheet = sheet.replace('nan', '')
+        for index, row in sheet.iterrows():
+            if row[ip] == '':
+                logger.error('%s: Ip not set for %s {}.' % (sheet_name, row))
+                continue
 
-# ---------------------------------------------------------------------
-sheet = pandas.read_excel(SPREADSHEET, sheet_name=SHEET_MBTEMP, dtype=str)
-sheet = sheet.replace('nan', '')
-for index, row in sheet.iterrows():
-    if row['IP'] == '':
-        logger.error('Ip not set for {}'.format(row))
-        continue
+            if aditional_check and not aditional_check(row, sheet_name):
+                continue
 
-    if row['IP'] in DATA_MBTEMP:
-        DATA_MBTEMP[row['IP']].append(row)
-    else:
-        DATA_MBTEMP[row['IP']] = [row]
-# ---------------------------------------------------------------------
-sheet = pandas.read_excel(SPREADSHEET, sheet_name=SHEET_4UHV, dtype=str)
-sheet = sheet.replace('nan', '')
-for index, row in sheet.iterrows():
+            if row[ip] in self.data:
+                self.data[row[ip]].append(row)
+            else:
+                self.data[row[ip]] = [row]
+        logger.info('Generated data structure from sheet %s.' % sheet_name)
 
-    if row['IP'] == '':
-        logger.error('4UHV: Ip not set for {}'.format(row))
-        continue
-
-    if row['IP'] in DATA_4UHV:
-        DATA_4UHV[row['IP']].append(row)
-    else:
-        DATA_4UHV[row['IP']] = [row]
-
-# ---------------------------------------------------------------------
-sheet = pandas.read_excel(SPREADSHEET, sheet_name=SHEET_MKS, dtype=str)
-sheet = sheet.replace('nan', '')
-for index, row in sheet.iterrows():
-    if row['IP'] == '':
-        logger.error('MKS: Ip not set for {}'.format(row))
-        continue
+def mks_check(row, sheet_name):
     if row['Configuracao'] == '':
-        logger.error('MKS: Configuration not set for {}'.format(row))
-        continue
+        logger.error('%s: Configuration not set for {}'.format(sheet_name, row))
+        return False
+    return True
 
-    if row['IP'] in DATA_MKS:
-        DATA_MKS[row['IP']].append(row)
-    else:
-        DATA_MKS[row['IP']] = [row]
-# ---------------------------------------------------------------------
+'''
+    Data structures will contain a vector of entries related to a single IP
+'''
+DATA_MBTEMP = DbData(SHEET_MBTEMP).data
+DATA_4UHV = DbData(SHEET_4UHV).data
+DATA_MKS = DbData(SHEET_MKS, aditional_check=mks_check).data
+DATA_COUNTING_PRU = DbData(SHEET_COUNTING_PRU).data
