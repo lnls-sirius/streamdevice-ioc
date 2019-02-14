f130cd57b16390d7b0ab5dc6be04b94e7203a009
carneirofc <claudiofcarneiro@hotmail.com>
2019-02-14 09:30:07 -0200

    Removed unused docker folder inside mks.

--- mks937b/docker/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. ./conf-env.sh
-echo ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG}
-docker run -d \
-    --network host \
-    --name ${CONTAINER_NAME} \
-     ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG}
