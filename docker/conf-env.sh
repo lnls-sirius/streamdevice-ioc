69a8d3dc57f9efc605c5f75e4c605779ec65df5a
carneirofc <claudiofcarneiro@hotmail.com>
2019-02-15 09:49:53 -0200

    Docker related files.

--- docker/conf-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DOCKER_MANTAINER_NAME=lnlscon
-DOCKER_NAME=streamdevice-ioc
-DOCKER_TAG=latest
--- docker/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-. ./conf-env.sh
-echo ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG}
-docker run -d --name ${CONTAINER_NAME} ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG}
