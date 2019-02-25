26187728634d1f17c7f315580db617003f04fd02
carneirofc <claudiofcarneiro@hotmail.com>
2019-02-25 14:39:49 -0300

    docker: current build version.

--- docker/build.sh
+++ docker/build.sh
@@ -6,5 +6,5 @@ DOCKER_NAME=streamdevice-ioc
 
 # The tag pattern :
 # v.major.minor-of-the-day.day.month.year
-DOCKER_TAG=v1.0.19.2.2019
-docker build -t ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG} .
+DOCKER_TAG=v1.1.25.2.2019
+docker build --no-cache -t ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG} .
