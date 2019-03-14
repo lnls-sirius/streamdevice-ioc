98ee4c466fd20ffd6124c310a6185e150d151ee1
carneirofc <claudiofcarneiro@hotmail.com>
2019-03-14 12:26:06 -0300

    Support for socket bridge.

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,14 @@ source common/functions
 echo Folder ${1}\; Prefix ${2}\; BASE PORT ${3};
 
 build ${1} ${2} ${3};
-run_ioc ${2};
+
+# Check if it will run using a tcp<->unix socket bridge
+set -x
+if [[ -z "${SOCKET_BRIDGE}" ]]; then
+  run_ioc ${2};
+else
+  run_ioc_unix_bridge ${2};
+fi
+set +x
 
 tail -f /dev/null
