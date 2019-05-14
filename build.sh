588b9c8365eef8de0b528cdad794d1d556ae30f9
carneirofc <claudiofcarneiro@hotmail.com>
2019-05-14 16:59:58 -0300

    build: Fix tag name

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ DOCKER_MANTAINER_NAME=lnlscon
 DOCKER_NAME=streamdevice-ioc
 
 # The tag pattern :
-DOCKER_TAG=asyn3.35
+DOCKER_TAG=asyn3.35_StreamDevice2.8.8
 docker build -t ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}:${DOCKER_TAG}   .
