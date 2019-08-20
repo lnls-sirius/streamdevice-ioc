14b3acd95ea61799e1bf6eceb2b8f81809f809c7
carneirofc <claudiofcarneiro@hotmail.com>
2019-08-20 09:06:48 -0300

    spixconv: Removed default arg values

--- spixconv/generate.py
+++ spixconv/generate.py
@@ -22,9 +22,9 @@ if __name__ == "__main__":
     parser.add_argument('--epics-ca-port-increase', help='Increase EPICS_CA_SERVER_PORT by 2 for each deployed IOC.', action='store_true')
     parser.add_argument('--base-epics-ca-port', help='Initial EPICS CA server port. It will increase by 2 for every ioc.', type=int, default=5064)
     parser.add_argument('--cmd-prefix', default='SPIxCONV', help='Prefix for the .cmd files.')
-    parser.add_argument('--epics-base', default='/opt/epics-R3.15.5/base', help='Epics base path.')
-    parser.add_argument('--asyn', default='/opt/epics-R3.15.5/modules/asyn4-33', help='Asyn driver path.')
-    parser.add_argument('--top', default='/opt/stream-ioc', help='Stream-ioc path.')
+    parser.add_argument('--epics-base', help='Epics base path.')
+    parser.add_argument('--asyn', help='Asyn driver path.')
+    parser.add_argument('--top', help='Stream-ioc path.')
     parser.add_argument('--arch', help='System architecture.', choices=['linux-x86_64', 'linux-arm'], default='linux-x86_64')
     args = parser.parse_args()
 
