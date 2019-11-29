21f08e908b7ccfa89ed6d1de8fc4c11bbb76c1ce
carneirofc <claudiofcarneiro@hotmail.com>
2019-11-29 17:51:52 -0300

    bin: entrypoints.

--- bin/mbtemp.sh
+++ bin/mbtemp.sh
@@ -3,9 +3,10 @@ set -e
 source common/functions
 
 export BASE_PROCSERV_PORT=20700
-PREFIX=MBTemp-
+PREFIX=MBTemp
 
-./mbtemp/generate.py --epics-base ${EPICS_BASE} --asyn ${ASYN} --cmd-prefix ${PREFIX} \
+./common/generate.py --epics-base ${EPICS_BASE} --asyn ${ASYN} --device ${PREFIX} \
     --epics-log-addr ${EPICS_IOC_LOG_INET} --epics-log-port ${EPICS_IOC_LOG_PORT} \
     --top ${TOP} ${EPICS_CA_PORT_INCREASE} ${BASE_EPICS_CA_PORT}
-run  ${PREFIX}
+
+run  ${PREFIX}-
