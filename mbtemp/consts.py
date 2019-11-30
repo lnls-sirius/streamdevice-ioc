da84efd455f99afe51f62a478855dba9f674a0af
carneirofc <claudiofcarneiro@hotmail.com>
2019-11-30 08:32:43 -0300

    mbtemp: Csv git.

--- mbtemp/consts.py
+++ mbtemp/consts.py
@@ -1,3 +1,7 @@
 from common.db import DbData
+from common.utils import dump_to_history
+
 SHEET_MBTEMP = 'PVs MBTemp'
-DATA_MBTEMP = DbData(SHEET_MBTEMP).data
+_mbt = DbData(SHEET_MBTEMP)
+DATA_MBTEMP = _mbt.data
+dump_to_history(_mbt.sheet, 'mbtemp.csv')
