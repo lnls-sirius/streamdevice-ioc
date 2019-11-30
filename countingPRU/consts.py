3ca0801b7125df0ca620236da609d4a191acf19f
carneirofc <claudiofcarneiro@hotmail.com>
2019-11-30 09:53:49 -0300

    Counting PRU: Auto git commit.

--- countingPRU/consts.py
+++ countingPRU/consts.py
@@ -1,3 +1,7 @@
 from common.db import DbData
+from common.utils import dump_to_history
+
 SHEET_COUNTING_PRU = 'PVs Counting PRU'
-DATA_COUNTING_PRU = DbData(SHEET_COUNTING_PRU).data
+_pru = DbData(SHEET_COUNTING_PRU)
+DATA_COUNTING_PRU = _pru.data
+dump_to_history(_pru.sheet, 'counting-pru.csv')
