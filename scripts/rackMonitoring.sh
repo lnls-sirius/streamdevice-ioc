e548569dd36114717be43fa0b72f9ccbc9fa4de2
carneirofc <claudiofcarneiro@hotmail.com>
2020-09-03 13:34:58 -0300

    Update docker images tag

--- scripts/rackMonitoring.sh
+++ scripts/rackMonitoring.sh
@@ -2,7 +2,7 @@
 set -e
 source common/functions
 
-export BASE_PROCSERV_PORT=21100
+export BASE_PROCSERV_PORT=
 PREFIX=RackMonitoring
 
 ./common/generate.py --epics-base ${EPICS_BASE} --asyn ${ASYN} --device ${PREFIX} \
