892d7ee61564a142a31159b73ae49c6a16b95c3a
carneirofc <claudiofcarneiro@hotmail.com>
2021-02-25 11:36:18 -0300

    Update spreadsheet and aginlent devices

--- scripts/agilent4uhv/server/cmd/UHV-10.128.101.102:5004.cmd
+++ scripts/agilent4uhv/server/cmd/UHV-10.128.101.102:5004.cmd
@@ -29,11 +29,11 @@ dbLoadRecords("db/Agilent-4UHV-Channel.db", "PORT=IPPort0, P=BO-03U:VA-SIP20-BG,
 dbLoadRecords("db/Agilent-4UHV-Channel.db", "PORT=IPPort0, P=BO-03U:VA-SIP20-ED,D=BO-RA01:VA-SIPC-02,CH_NUM=4,ADDR=140")
 
 # Device 3
-dbLoadRecords("db/Agilent-4UHV-Device.db", "PORT=IPPort0,P=SR-RA01:VA-SIPC-03,P_CH1=SI-01SA:VA-SIP20-BG,P_CH2=SI-01SA:VA-SIP20-MD01,P_CH3=SI-01SA:VA-SIP20-MD02,P_CH4=SI-01SA:VA-SIP20-MD03,E_CH1=,E_CH2=,E_CH3=,E_CH4=,ADDR=141,PHAS=3,TIME=6")
-dbLoadRecords("db/Agilent-4UHV-Channel.db", "PORT=IPPort0, P=SI-01SA:VA-SIP20-BG,D=SR-RA01:VA-SIPC-03,CH_NUM=1,ADDR=141")
-dbLoadRecords("db/Agilent-4UHV-Channel.db", "PORT=IPPort0, P=SI-01SA:VA-SIP20-MD01,D=SR-RA01:VA-SIPC-03,CH_NUM=2,ADDR=141")
-dbLoadRecords("db/Agilent-4UHV-Channel.db", "PORT=IPPort0, P=SI-01SA:VA-SIP20-MD02,D=SR-RA01:VA-SIPC-03,CH_NUM=3,ADDR=141")
-dbLoadRecords("db/Agilent-4UHV-Channel.db", "PORT=IPPort0, P=SI-01SA:VA-SIP20-MD03,D=SR-RA01:VA-SIPC-03,CH_NUM=4,ADDR=141")
+dbLoadRecords("db/Agilent-4UHV-Device.db", "PORT=IPPort0,P=SI-RA01:VA-SIPC-03,P_CH1=SI-01SA:VA-SIP20-BG,P_CH2=SI-01SA:VA-SIP20-MD01,P_CH3=SI-01SA:VA-SIP20-MD02,P_CH4=SI-01SA:VA-SIP20-MD03,E_CH1=,E_CH2=,E_CH3=,E_CH4=,ADDR=141,PHAS=3,TIME=6")
+dbLoadRecords("db/Agilent-4UHV-Channel.db", "PORT=IPPort0, P=SI-01SA:VA-SIP20-BG,D=SI-RA01:VA-SIPC-03,CH_NUM=1,ADDR=141")
+dbLoadRecords("db/Agilent-4UHV-Channel.db", "PORT=IPPort0, P=SI-01SA:VA-SIP20-MD01,D=SI-RA01:VA-SIPC-03,CH_NUM=2,ADDR=141")
+dbLoadRecords("db/Agilent-4UHV-Channel.db", "PORT=IPPort0, P=SI-01SA:VA-SIP20-MD02,D=SI-RA01:VA-SIPC-03,CH_NUM=3,ADDR=141")
+dbLoadRecords("db/Agilent-4UHV-Channel.db", "PORT=IPPort0, P=SI-01SA:VA-SIP20-MD03,D=SI-RA01:VA-SIPC-03,CH_NUM=4,ADDR=141")
 
 set_pass0_restoreFile("$(TOP)/autosave/save/10_128_101_102:5004.sav")
 set_pass1_restoreFile("$(TOP)/autosave/save/10_128_101_102:5004.sav")
